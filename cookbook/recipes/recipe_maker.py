cc46ecd239f7faba106b9e4da5ba57a32d179cbf
Kathryn Lingel <kathryn@lingel.net>
2016-06-10 22:57:07 -0700

    refactor 1/many

--- cookbook/recipes/recipe_maker.py
+++ cookbook/recipes/recipe_maker.py
@@ -3,74 +3,96 @@ import requests
 
 debug = False
 
+measurement_keywords = [
+    "cup",
+    "dash",
+    "lb",
+    "liter",
+    "litre",
+    "ounce",
+    "oz",
+    "pinch",
+    "pound",
+    "tbsp",
+    "tsp",
+]
+direction_keywords = [
+    "add",
+    "bake",
+    "boil",
+    "brown",
+    "chop",
+    "cube",
+    "degrees",
+    "heat",
+    "slice",
+    "simmer",
+    "stir",
+    "until",
+]
+
 def get_parsed(url):
     page = requests.get(url)
     tree = html.fromstring(page.content)
     return tree
 
-def find_ingredients(tree):
-    measurement_keywords = [
-        "cup",
-        "dash",
-        "lb",
-        "liter",
-        "litre",
-        "ounce",
-        "oz",
-        "pinch",
-        "pound",
-        "tbsp",
-        "tsp",
-    ]
+def keyword_score(tree, keywords):
+    try:
+        content = tree.text_content().lower()
+    except ValueError:
+        return 0
+    return sum(map(lambda k: content.count(k), keywords))
 
-    def ingredient_score(e):
-        try:
-            content = e.text_content().lower()
-        except ValueError:
-            return 0
-        return sum(map(lambda k: content.count(k), measurement_keywords))
+def find_deepest_nested_keywords(tree, keywords):
+    x = keyword_score(tree, keywords)
+    print(tree, x)
+    if debug:
+        print(x)
+    if x == 0:
+        return None
+    elif not tree.getchildren():
+        return tree
 
-    def find_deepest_nested_ingredients(e):
-        x = ingredient_score(e)
+    best_child_score = x/2  # we're looking for better than 50% in one place
+    best_child = tree
+
+    for c in tree.iterchildren():
         if debug:
-            print(x)
-        if x == 0:
-            return None
-        elif not e.getchildren():
-            return e
+            print("child", c)
+        if keyword_score(c, keywords) == 0:  # mothing to see here, movee along
+            continue
 
-        best_child_score = x
-        best_child = e
+        best_in_branch = find_deepest_nested_keywords(c, keywords)
+        if best_in_branch is None:  # should not hit this case bc we're only
+                                    # going into branches that score nonzero
+            print("wtf happened")
+            continue
 
-        for c in e.iterchildren():
+        branch_score = keyword_score(best_in_branch, keywords)
+        if branch_score >= best_child_score:
             if debug:
-                print("child", c)
-            if ingredient_score(c) == 0:
-                continue
-            best_in_branch = find_deepest_nested_ingredients(c)
-            if best_in_branch is None:  # should not hit this case bc we're only
-                                        # going into branches that score nonzero
-                print("wtf happened")
-                continue
-            branch_score = ingredient_score(best_in_branch)
-            if branch_score >= best_child_score:
-                if debug:
-                    print("replacing")
-                best_child_score = branch_score
-                best_child = best_in_branch
+                print("replacing")
+            best_child_score = branch_score
+            best_child = best_in_branch
 
-        if best_child is None:
-            print("Could not identify ingredients through common quantities")
+    if best_child is None:
+        print("Could not find keywords")
 
-        return best_child
+    return best_child
     
-    return find_deepest_nested_ingredients(tree.getchildren()[1])
-
 
 if __name__ == "__main__":
     tree = get_parsed('http://pixelatedcrumb.com/2011/03/06/prosciutto-and-sweet-potato-risotto')
-    ingredients_block = find_ingredients(tree)
+    body = tree.getchildren()[1]  # TODO: this but better
+
+    ingredients_block = find_deepest_nested_keywords(body, measurement_keywords)
     if ingredients_block is not None:
-        print(ingredients_block.text_content())
+        print(ingredients_block.text_content().strip())
     else:
-        print("ingredients not found")
+        print("could not identify ingredients by looking for common quantities")
+
+    directions_block = find_deepest_nested_keywords(body, direction_keywords)
+    if directions_block is not None:
+        print(directions_block.text_content().strip())
+    else:
+        print("could not identify directions by looking for common directives")
